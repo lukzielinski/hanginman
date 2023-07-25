defb7e45d7a15806788157d7620cd0299adc8b80
ReQ <mrzielony22@gmail.com>
2023-07-25 20:59:38 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Router from 'svelte-spa-router'
+
+    const routes = {}
+</script>
+
+<svelte:head>
+    <link rel="icon" type="image/svg" href="/assets/robotIcon.svg" />
+    <title>Hang Man</title>
+</svelte:head>
+
+<main>
+    <Router {routes} />
+</main>
+
+<style lang="less">
+    main {
+        text-align: center;
+    }
+</style>
